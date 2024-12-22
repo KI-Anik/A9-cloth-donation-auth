5fc852cc3eaf9c0601aa47322ce6d999e26da7af
KI-Anik <iamkhairul101@gmail.com>
2024-12-22 12:37:08 +0600

    extraTwosec.jsx done

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -14,11 +14,15 @@ import '../index.css';
 
 // import required modules
 import { Navigation } from 'swiper/modules';
+import { FaSearch, FaWpforms } from "react-icons/fa";
+import { GiConfirmed } from "react-icons/gi";
+import { FaMapLocation } from "react-icons/fa6";
+import ExtraTwoSec from "./Pages/Home/ExtraTwoSec";
 
 export default function Banner() {
   return (
     <>
-    {/* index.css */}
+      {/* index.css */}
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
 
         <SwiperSlide> <img src={banner1} alt="cloth-photo1" /> </SwiperSlide>
@@ -27,8 +31,8 @@ export default function Banner() {
 
       </Swiper>
 
-{/* 3rd section */}
-      <div className="space-y-6 bg-blue-200 p-12 text-center">
+      {/* 3rd section */}
+      <section className="space-y-6 bg-slate-300 p-12 text-center">
         <h2 className="text-4xl font-bold">What we offer!</h2>
         <div className="space-y-6 text-xl">
           <p>
@@ -38,26 +42,100 @@ export default function Banner() {
             Have you ever asked yourself: Which are the organizations where I can give away my old household items? Where can I donate clothes? Can someone pick up the donations from my home/office? Can I know the requirements of the NGOs near me and fulfil them on my birthday? </p>
 
           <p>
-          You have questions and BDCOT Step has the answers! Just surf around our website and you will learn everything you need to know to make a donation. Whenever you are ready, we will even help you schedule a donation pick-up. </p>
-      </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 pt-14 ">
-        <img src={banner4} alt="" />
-        <div className="space-y-5">
-        <h3 className="text-3xl font-semibold">Your gift will bring smiles to more children like Sahar</h3>
-        <p>Amidst an ongoing conflict and back-to-back earthquakes, families like Sahar’s are living in a state of constant stress and cannot afford basic <br />essentials. And with winter temperatures in many areas of Afghanistan dropping as low as -20 degrees Celsius, it’s critical that children receive warm clothes and blankets in time. Without them, children are at risk of developing life-threatening illnesses, like pneumonia. <br />
-
-UNICEF is preparing warm winter kits with clothing and blankets for children and their families in need. But the needs are immense and we cannot do this without your help. <br />
-
-Donate now to help a child stay warm this winter.</p> <br />
-
-{/* btn here */}
-<button className="btn bg-orange-400 hover:text-green-600 text-white text-xl font-bold">Send Warmth Now</button> 
-
-<p>Millions of children around the world are counting on our support to stay warm this winter. With your help, we can ensure they have the essentials they need to make it through the harsh, cold months ahead.</p>
+            You have questions and BDCOT Step has the answers! Just surf around our website and you will learn everything you need to know to make a donation. Whenever you are ready, we will even help you schedule a donation pick-up. </p>
         </div>
-      </div>
-    </div >
+
+        <div className="grid grid-cols-1 md:grid-cols-2 pt-14 ">
+          <img src={banner4} alt="" />
+          <div className="space-y-5">
+            <h3 className="text-3xl font-semibold">Your gift will bring smiles to more children like Sahar</h3>
+            <p>Amidst an ongoing conflict and back-to-back earthquakes, families like Sahar’s are living in a state of constant stress and cannot afford basic <br />essentials. And with winter temperatures in many areas of Afghanistan dropping as low as -20 degrees Celsius, it’s critical that children receive warm clothes and blankets in time. Without them, children are at risk of developing life-threatening illnesses, like pneumonia. <br />
+
+              BDCOT is preparing warm winter kits with clothing and blankets for children and their families in need. But the needs are immense and we cannot do this without your help. <br />
+
+              Donate now to help a child stay warm this winter.</p> <br />
+
+            {/* btn here */}
+            <button className="btn bg-orange-400 hover:text-green-600 text-white text-xl font-bold">Send Warmth Now</button>
+
+            <p>Millions of children around the world are counting on our support to stay warm this winter. With your help, we can ensure they have the essentials they need to make it through the harsh, cold months ahead.</p>
+          </div>
+        </div>
+      </section >
+
+      {/* how it works start */}
+      <section className="bg-slate-200 space-y-4 text-center p-8">
+        <h2 className="text-2xl font-bold">How it works</h2>
+        <p>Donate winter clothing in just 3 steps and bring  warmth to this need</p>
+
+        <div className="grid md:grid-cols-3 justify-center items-center gap-5">
+          {/* 1st card */}
+          <div className="p-5 bg-white shadow-xl space-y-2">
+            <FaSearch className="text-3xl mx-auto"></FaSearch>
+            <h2 className="text-xl font-semibold">step 1: Find a Camp</h2>
+            <p>select a donation camp near you and view the details.</p>
+          </div>
+
+          {/*2nd card */}
+          <div className="p-5 bg-white shadow-xl space-y-2">
+            <FaWpforms className="text-3xl mx-auto"></FaWpforms>
+            <h2 className="text-xl font-semibold">step 2: Fill the form</h2>
+            <p>provide the neccessary details about your donation. </p>
+          </div>
+
+          {/* 3rd card */}
+          <div className="p-5 bg-white shadow-xl space-y-2">
+            <GiConfirmed className="text-3xl mx-auto"></GiConfirmed>
+            <h2 className="text-xl font-semibold">step 3: Confirm Donation</h2>
+            <p>submit your donation and receive a confirmation message.</p>
+          </div>
+
+          {/* all district */}
+          <div className="col-span-3">
+            <ul className="grid grid-cols-4">
+              <li className="flex justify-center items-center gap-3 space-y-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Dhaka</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Chattogram</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Barishal</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Sylhet</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Khulna</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Rangpur</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Mymensing</h5>
+              </li>
+              <li className="flex justify-center items-center gap-3">
+                <FaMapLocation></FaMapLocation>
+                <h5 className="font-semibold">Rajshahi</h5>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      {/* how it works end */}
+
+      {/* 4th section start */}
+      <section>
+        <ExtraTwoSec></ExtraTwoSec>
+      </section>
+      {/* 4th section end */}
     </>
   );
 }
---
+++ src/components/Pages/Home/ExtraTwoSec.jsx
@@ -0,0 +1,110 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
+
+const ExtraTwoSec = () => {
+    return (
+        <div className='text-center'>
+
+            <h2 className="text-3xl text-sky-600 font-bold  my-14">We couldn&apos;t have done it without the donors like you</h2>
+
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                <SwiperSlide>
+                    <div className=" p-8 pb-10 bg-white shadow-xl space-y-7">
+                        <p>Being a BDCOT donor is an honor and privilege for me. When you make a donation, you give hope and happiness to children who need it the most. This is what motivates me to continue donating to BDCOT.</p>
+                        <h4 className="font-semibold"> - Avishekh, a BDCOT Donor.</h4>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className=" p-8 pb-10 bg-white shadow-xl space-y-7">
+                        <p>I know that my donations will get to the right people and assist them in their trouble. It makes me feel that I have done something special for those in need. I also believe BDCOT to be a worthy organisation and one which will use the money efficiently where it is most needed.</p>
+
+
+                        <h4 className="font-semibold"> - Gashubije, a BDCOT Donor.</h4>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className=" p-8 pb-10 bg-white shadow-xl space-y-7">
+                        <p>It seemed to me, after working with various other organizations, that BDCOT is the most focused on children's needs and the most able. I feel I am able to make a difference no matter how big the task is with an organisation that never gives up!</p>
+
+                        <h4 className="font-semibold"> - Mainus, a BDCOT Donor.</h4>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className=" p-8 pb-10 bg-white shadow-xl space-y-7">
+                        <p>I was once a victim of a crisis. The experience was tough. The man who accommodated us treated us nicely. I know there are a lot of people out there who need help daily. To me, it doesn't matter whether you have all or not. With the little you have; you can put a smile on someone else's face.</p>
+
+                        <h4 className="font-semibold"> - George, a BDCOT Donor.</h4>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className=" p-8 pb-10 bg-white shadow-xl space-y-7">
+                        <p>I know that my donations will get to the right people and assist them in their trouble. It makes me feel that I have done something special for those in need. I also believe BDCOT to be a worthy organisation and one which will use the money efficiently where it is most needed.</p>
+
+                        <h4 className="font-semibold"> - washington, a BDCOT Donor.</h4>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+
+            <button className='btn bg-orange-400 hover:text-green-600 text-white text-xl my-8 font-bold'>Donate Now</button>
+
+
+            {/* LAST SECTION start */}
+            <section className='space-y-6 bg-slate-300 p-12 text-center'>
+                <h2 className="text-5xl text-sky-600 font-bold  my-14">Frequently Asked Questions</h2>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" defaultChecked />
+                    <div className="collapse-title text-xl font-medium">What is BDCOT</div>
+                    <div className="collapse-content">
+                        <p>BDCOT is the United Nations Children&apos;s Fund, working to protect and promote the rights of every child, everywhere. We work to help the most vulnerable with health care and immunizations, safe water and sanitation, nutrition, education, emergency relief, and more.
+
+                            Thanks to support from donors like you, we have been able to make a difference for children in need, to save, nurture, and protect the most vulnerable. We are active in over 190 countries and territories and have helped save more children&apos;s lives than any other humanitarian organization.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">How can i make donation</div>
+                    <div className="collapse-content">
+                        <p>You can securely make a one-time or/and monthly donation online on our website on the Donate Now button.
+
+                            Your contribution may be of any amount with no limitations. No donation is too small, and any donation can help save children&apos;s lives.
+
+                            You choose for how long you wish to donate: monthly or only once, and you can easily amend the frequency and amount of your donation or cancel your online giving. In order to contribute to a fundamental change in the lives of vulnerable children, the longer you choose to get involved, the more powerful your donation will be. A child&apos;s life can improve with your long-term, constant support. By donating, you become an active part of the solution and of the change process.</p>
+                    </div>
+                </div>
+                <div className="collapse collapse-arrow bg-base-200">
+                    <input type="radio" name="my-accordion-2" />
+                    <div className="collapse-title text-xl font-medium">How will i know how my donation is being utilized?</div>
+                    <div className="collapse-content">
+                        <p>We will be sending you regular updates in the form of e-mails, digital communication, etc., of the progress that has been made and keep you updated about BDCOT&apos;s work, in the region, and around the world.</p>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    );
+}
+
+
+export default ExtraTwoSec;
