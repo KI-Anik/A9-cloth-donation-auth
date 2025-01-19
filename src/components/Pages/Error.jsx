eec0672c7251d073bb00a3081f647e59da961f0d
KI-Anik <iamkhairul101@gmail.com>
2025-01-19 11:08:33 +0600

    Update Error page layout

--- src/components/Pages/Error.jsx
+++ src/components/Pages/Error.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div className='flex justify-center items-center min-h-screen'>
+        <div className='flex flex-col space-y-5 justify-center items-center min-h-screen'>
             <h2 className="text-5xl text-red-500 font-bold">Sorry content Not Found</h2>
+            <Link className="btn btn-accent text-xl" to={'/'}>Back to Home</Link>
         </div>
     );
 };
